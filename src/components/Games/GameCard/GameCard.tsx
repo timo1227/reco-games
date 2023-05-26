e1de03fbb6a1d71067a6cdf6ac9f85a0cdc8dc53
Tim <tim_1227@aol.com>
2023-05-25 21:10:51 -0400

    Add/Remove Button

--- src/components/Games/GameCard/GameCard.tsx
+++ src/components/Games/GameCard/GameCard.tsx
@@ -1,10 +1,14 @@
 'use client'
 
+import { useState } from 'react'
 import Image from 'next/image'
+import { Minus, Plus } from 'lucide-react'
 
 import { Games } from '@/types/Games'
 import getCroppedImgUrl from '@/lib/image-url'
 import { cn } from '@/lib/utils'
+import useGameContext from '@/hooks/useGameContext'
+import { Button } from '@/components/ui/button'
 import {
   Card,
   CardContent,
@@ -12,6 +16,12 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip'
 import { useToast } from '@/components/ui/use-toast'
 
 import { GamePlatforms } from './GamePlatforms'
@@ -21,7 +31,11 @@ interface Props {
 }
 
 export const GameCard = ({ game }: Props) => {
+  const [isHovered, setIsHovered] = useState(false)
+  const { games: userGames, setGames } = useGameContext()
   const { toast } = useToast()
+
+  const isInLibrary = userGames.find((g) => g.id === game.id)
 
   const onCardClick = () => {
     toast({
@@ -30,10 +44,39 @@ export const GameCard = ({ game }: Props) => {
       variant: 'destructive',
     })
   }
+
+  const onAddToLibrary = () => {
+    setGames((prev) => [...prev, game])
+  }
+
+  const onRemoveFromLibrary = () => {
+    setGames((prev) => prev.filter((g) => g.id !== game.id))
+  }
+
+  const onAddRemoveFromLibrary = () => {
+    if (isInLibrary) {
+      onRemoveFromLibrary()
+      toast({
+        title: 'Removed from library',
+        description: `${game.name} has been removed from your library`,
+      })
+    } else {
+      onAddToLibrary()
+      toast({
+        title: 'Added to library',
+        description: `${game.name} has been added to your library`,
+      })
+    }
+  }
+
   return (
     <Card
-      className={cn('h-[375px] w-[250px] md:w-[300px]', 'overflow-hidden px-0')}
-      onClick={() => onCardClick()}
+      className={cn(
+        'h-[375px] w-[250px] md:w-[300px]',
+        ' relative overflow-hidden px-0'
+      )}
+      onMouseOver={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
       <CardHeader className={cn('mb-5 h-[65%] p-0')}>
         <Image
@@ -44,15 +87,52 @@ export const GameCard = ({ game }: Props) => {
           className='h-full w-full object-cover'
         />
       </CardHeader>
-      <CardContent>
+      <CardContent className='absolute'>
         {game.parent_platforms && (
           <GamePlatforms
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
         )}
-        <CardTitle className='text-xl font-bold'>{game.name}</CardTitle>
+        <CardTitle
+          className='text-xl font-bold hover:cursor-pointer hover:underline'
+          onClick={() => onCardClick()}
+        >
+          {game.name}
+        </CardTitle>
       </CardContent>
-      <CardFooter className='hidden sm:block'></CardFooter>
+      <CardFooter
+        className={cn(
+          !isHovered ? 'hidden' : 'block animate-in',
+          'absolute bottom-1 right-1 max-w-fit p-0'
+        )}
+      >
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button
+                variant='outline'
+                className=' w-10 rounded-full p-0 dark:border-gray-700'
+                onClick={() => onAddRemoveFromLibrary()}
+              >
+                {isInLibrary ? (
+                  <>
+                    <Minus className='h-4 w-4' />
+                    <span className='sr-only'>Minus</span>
+                  </>
+                ) : (
+                  <>
+                    <Plus className='h-4 w-4' />
+                    <span className='sr-only'>Add</span>
+                  </>
+                )}
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>
+              {isInLibrary ? 'Remove from library' : 'Add to library'}
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </CardFooter>
     </Card>
   )
 }
