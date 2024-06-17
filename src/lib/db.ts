a9109b6edfa688809dc7cfa63d782e0e6a67a11e
Tim Estrada <the4@njit.edu>
2024-06-17 02:25:42 +0000

    chore: add prisma

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,34 +1,17 @@
-// This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
-import { MongoClient } from 'mongodb';
+import { PrismaClient } from '@prisma/client';
 
-if (!process.env.MONGODB_URI) {
-  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
-}
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
 
-const URI = process.env.MONGODB_URI;
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
 
-let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
 
-if (process.env.NODE_ENV === 'development') {
-  // In development mode, use a global variable so that the value
-  // is preserved across module reloads caused by HMR (Hot Module Replacement).
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
-  const globalWithMongo = global as typeof globalThis & {
-    _mongoClientPromise: Promise<MongoClient>;
-  };
+export const db = prisma;
 
-  if (!globalWithMongo._mongoClientPromise) {
-    client = new MongoClient(URI);
-    globalWithMongo._mongoClientPromise = client.connect();
-  }
-  clientPromise = globalWithMongo._mongoClientPromise;
-} else {
-  // In production mode, it's best to not use a global variable.
-  client = new MongoClient(URI);
-  clientPromise = client.connect();
-}
-
-// Export a module-scoped MongoClient promise. By doing this in a
-// separate module, the client can be shared across functions.
-export const db = clientPromise;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
